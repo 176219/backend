2afca88aeba400211f76b859f025ba1e0aeca198
176219 <azriellanto123@gmail.com>
2026-05-22 02:04:21 +0800

    fix

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 const router = express.Router();
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
 // Load the swagger.yaml file from the project root
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+
+swaggerDocument.servers = [
+  {
+    url: process.env.BASE_URL || 'http://localhost:4000',
+    description: 'API server',
+  },
+];
 
 // Mount Swagger UI on the root path of this router
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
