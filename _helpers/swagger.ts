223c247bf8a19a1098303db3ebfbfafda3313696
176219 <azriellanto123@gmail.com>
2026-05-22 02:22:47 +0800

    HOHAAAY

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -5,15 +5,21 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
 // Load the swagger.yaml file from the project root
-const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
-
-swaggerDocument.servers = [
-  {
-    url: process.env.BASE_URL || 'http://localhost:4000',
-    description: 'API server',
+const swaggerDocument = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Node.js Sign-up and Verification API',
+    description: 'Node.js + MySQL - API with email sign-up, verification, authentication and forgot password',
+    version: '1.0.0',
   },
-];
-
+  servers: [
+    {
+      url: process.env.BASE_URL || 'http://localhost:4000',
+      description: 'API server',
+    },
+  ],
+  
+};
 // Mount Swagger UI on the root path of this router
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
